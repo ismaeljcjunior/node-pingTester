a51de996f19ebe5d18727694a989a43426c261b7
Ismael Junior <ismaeljcjunior@gmail.com>
2023-02-08 13:46:10 -0300

    feat

--- src/server.js
+++ src/server.js
@@ -1,20 +1,32 @@
-const exec = require('child_process').exec
-const fs = require('fs')
+
+
+const exec = require("child_process").exec;
+const fs = require("fs");
 
 const hosts = ['www.google.com', '10.50.2.181','10.50.2.250']
-const replyFromLocale = 'Reply from'
+const replyFromLocale = "Reply from";
 
-const promises = []
+const promises = [];
 
-hosts.forEach((host) => {
+hosts.forEach(host => {
     promises.push(new Promise((resolve, reject) => {
         exec(`ping -n 1 -w 1000 ${host}`, (err, stdout, stderr) => {
-            
-            let status= "offline"
-            let output = stderr.toString()
-            let replyFromIndex = output.indexOf(replyFromLocale)
-            if(replyFromIndex > 0 && output.substring(replyFromIndex).toUpperCase().indexOf('BYTES' > 0))
+            // successfull response: Reply from 142.250.186.78: bytes=32 time=10ms TTL=128
+            // unsuccessfull:        Reply from 192.168.1.3: Destination host unreachable
+            // host unreachable:     Ping request could not find host
+            let status = "offline";
+            let output = stdout.toString();
+            let replyFromIndex = output.indexOf(replyFromLocale);
+            if (replyFromIndex > 0 && output.substring(replyFromIndex).toUpperCase().indexOf("BYTES") > 0) {
+                status = "online"
+            }
+            resolve(new Date().toISOString() + " " + host + " " + status)
         })
-        
     }))
+})
+
+Promise.all(promises).then((results) => {
+    fs.writeFile("ping-results.txt", results.join("\n"), (err) => {
+        if (err) { console.log(err); }
+    })
 })
